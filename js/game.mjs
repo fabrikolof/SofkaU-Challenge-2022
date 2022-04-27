19745754c2442c221f27b4fd66b87b272d501e8b
Fabricio <fabribcom@gmail.com>
2022-04-27 00:52:55 -0300

    test

--- js/game.mjs
+++ js/game.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { random } from "../data/questions.mjs";
+console.log("funciona")
 import { Game } from "../models/Game.mjs";
 import { Player } from "../models/Player.mjs";
 import { UI } from "../models/UI.mjs";
 
-console.log("funciona")
 
 /**
  *
@@ -13,21 +13,30 @@ console.log("funciona")
  */
 const renderPage = (game, ui) => {
 
-  if (game.idEnded()) {
-    let nombre = sessionStorage.getItem("name");
-    let player = new Player(nombre);
-    player.saveScore(game.score);
-    ui.showScores(game.score)
-  } else {
+
+  //test
     ui.showQuestion(game.getQuestionIndex().text);
     ui.showOptions(game.getQuestionIndex().options, (currentOption) => {
       game.guess(currentOption);
       renderPage(game, ui);
     });
-    ui.showProgress(game.questionIndex + 1, game.questions.length);
-    ui.showActualScore(game.score);
-    ui.showLevel(game.questionIndex + 1)
-  }
+
+
+  // if (game.idEnded()) {
+  //   let nombre = sessionStorage.getItem("name");
+  //   let player = new Player(nombre);
+  //   player.saveScore(game.score);
+  //   ui.showScores(game.score)
+  // } else {
+  //   ui.showQuestion(game.getQuestionIndex().text);
+  //   ui.showOptions(game.getQuestionIndex().options, (currentOption) => {
+  //     game.guess(currentOption);
+  //     renderPage(game, ui);
+  //   });
+  //   ui.showProgress(game.questionIndex + 1, game.questions.length);
+  //   ui.showActualScore(game.score);
+  //   ui.showLevel(game.questionIndex + 1)
+  // }
 };
 
 function main() {
