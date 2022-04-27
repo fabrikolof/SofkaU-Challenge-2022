04caa0bc54e8314b863dd2c06f26d0ad6767ebda
Fabricio <fabribcom@gmail.com>
2022-04-27 00:35:20 -0300

    test

--- js/game.mjs
+++ js/game.mjs
@@ -1,47 +1,49 @@
-// // @ts-check
-// import { random } from "../data/questions.mjs";
-// import { Game } from "../models/Game.mjs";
-// import { Player } from "../models/Player.mjs";
-// import { UI } from "../models/UI.mjs";
-
-// /**
-//  *
-//  * @param {Game} game the main game object
-//  * @param {UI} ui ui object
-//  */
-// const renderPage = (game, ui) => {
-
-//   if (game.idEnded()) {
-//     let nombre = sessionStorage.getItem("name");
-//     let player = new Player(nombre);
-//     player.saveScore(game.score);
-//     ui.showScores(game.score)
-//   } else {
-//     ui.showQuestion(game.getQuestionIndex().text);
-//     ui.showOptions(game.getQuestionIndex().options, (currentOption) => {
-//       game.guess(currentOption);
-//       renderPage(game, ui);
-//     });
-//     ui.showProgress(game.questionIndex + 1, game.questions.length);
-//     ui.showActualScore(game.score);
-//     ui.showLevel(game.questionIndex + 1)
-//   }
-// };
-
-// function main() {
-//   const game = new Game(random());
-//   const ui = new UI();
-//   renderPage(game, ui);
-  
-// }
-
-
-// main();
-
-
-
+// @ts-check
+import { random } from "../data/questions.mjs";
+import { Game } from "../models/Game.mjs";
+import { Player } from "../models/Player.mjs";
+import { UI } from "../models/UI.mjs";
 
 console.log("funciona")
+
+/**
+ *
+ * @param {Game} game the main game object
+ * @param {UI} ui ui object
+ */
+const renderPage = (game, ui) => {
+
+  if (game.idEnded()) {
+    let nombre = sessionStorage.getItem("name");
+    let player = new Player(nombre);
+    player.saveScore(game.score);
+    ui.showScores(game.score)
+  } else {
+    ui.showQuestion(game.getQuestionIndex().text);
+    ui.showOptions(game.getQuestionIndex().options, (currentOption) => {
+      game.guess(currentOption);
+      renderPage(game, ui);
+    });
+    ui.showProgress(game.questionIndex + 1, game.questions.length);
+    ui.showActualScore(game.score);
+    ui.showLevel(game.questionIndex + 1)
+  }
+};
+
+function main() {
+  const game = new Game(random());
+  const ui = new UI();
+  renderPage(game, ui);
+  
+}
+
+
+main();
+
+
+
+
+
 
 
 document.getElementById("salir").addEventListener("click", () => {
