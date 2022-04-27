35388b13d73b825b9ead40cc6b33d8f6c8f3db84
Fabricio <fabribcom@gmail.com>
2022-04-27 02:11:18 -0300

    url fix

--- js/game.js
+++ js/game.js
@@ -1,15 +1,9 @@
 // @ts-check
 
-console.log("funciona 1")
 import { random } from "../data/questions.js";
-console.log("funciona 2")
 import { Game } from "../models/Game.js";
-console.log("funciona 3")
 import { Player } from "../models/Player.js";
-console.log("funciona 4")
 import { UI } from "../models/UI.js";
-console.log("funciona 5")
-
 
 /**
  *
@@ -17,31 +11,21 @@ console.log("funciona 5")
  * @param {UI} ui ui object
  */
 const renderPage = (game, ui) => {
-
-
-  //test
+  if (game.idEnded()) {
+    let nombre = sessionStorage.getItem("name");
+    let player = new Player(nombre);
+    player.saveScore(game.score);
+    ui.showScores(game.score)
+  } else {
     ui.showQuestion(game.getQuestionIndex().text);
     ui.showOptions(game.getQuestionIndex().options, (currentOption) => {
       game.guess(currentOption);
       renderPage(game, ui);
     });
-
-
-  // if (game.idEnded()) {
-  //   let nombre = sessionStorage.getItem("name");
-  //   let player = new Player(nombre);
-  //   player.saveScore(game.score);
-  //   ui.showScores(game.score)
-  // } else {
-  //   ui.showQuestion(game.getQuestionIndex().text);
-  //   ui.showOptions(game.getQuestionIndex().options, (currentOption) => {
-  //     game.guess(currentOption);
-  //     renderPage(game, ui);
-  //   });
-  //   ui.showProgress(game.questionIndex + 1, game.questions.length);
-  //   ui.showActualScore(game.score);
-  //   ui.showLevel(game.questionIndex + 1)
-  // }
+    ui.showProgress(game.questionIndex + 1, game.questions.length);
+    ui.showActualScore(game.score);
+    ui.showLevel(game.questionIndex + 1)
+  }
 };
 
 function main() {
